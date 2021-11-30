d9c227ae956397e7359ed55a2d4b1186073bf5ea
ChabaJosa <chabagjg@gmail.com>
2021-11-29 19:19:11 -0500

    Semi-solution

--- src/App.js
+++ src/App.js
@@ -1,33 +1,58 @@
 import logo from "./logo.svg";
 import StarPic from "./star.png";
 import "./App.css";
-
-import { useState } from "react";
-
+//
+import { useState, useRef } from "react";
+//
 function App() {
-  const [currentStar, setcurrentStar] = useState();
+  const starObj = useRef({ start: null, end: null });
+  const [hoverArr, setHoverArr] = useState([]);
   //
   const handleHover = async (e, num) => {
-    // console.log(e);
-    await setcurrentStar(num);
-    let starArr = document.getElementsByClassName("star");
-    // console.log(starArr[0].style)
-    if (starArr.length > 0) {
-      for (let i = currentStar; i === 0; i--) {
-        console.log(starArr[i]);
-        if (i >= num) {
-          starArr[i].style.filter = "none";
-        } else {
-          // return;
-        }
+    if (hoverArr.includes(num.num)) {
+      if (e.target.style.filter !== "none") {
+        e.target.style.filter = "none";
+      } else {
+        e.target.style.filter = "grayscale(var(--value, 100%))";
       }
     }
+  };
+  //
+  const handleClick = async (e, num) => {
+    console.log(starObj);
+    let count = num.num;
+    let newArr = [];
+    //
+    if (starObj.current.start === null) {
+      starObj.current.start = num.num;
+    } else {
+      starObj.current.end = num.num;
+    }
+    //
+    if (starObj.current.end === null) {
+      while (count !== 5) {
+        if (!hoverArr.includes(count)) {
+          newArr.push(count);
+        }
+        count++;
+      }
+      setHoverArr(newArr);
+      //
+      if (e.target.style.filter === "none") {
+        e.target.style.filter = "grayscale(var(--value, 100%))";
+      } else {
+        e.target.style.filter = "none";
+      }
+    }
+    //
+    console.log("hello", e.target.style.filter, hoverArr);
   };
   //
   const Star = (num) => {
     return (
       <div className={"star-container"}>
         <img
+          onClick={(e) => handleClick(e, num)}
           onMouseOver={(e) => handleHover(e, num)}
           src={StarPic}
           alt="Img"
@@ -36,7 +61,7 @@ function App() {
       </div>
     );
   };
-
+  //
   return (
     <div className="App">
       <header className="App-header">
